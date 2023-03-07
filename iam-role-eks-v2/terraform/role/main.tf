47a9e0eb3596a8cf0042596e9aca214b2973e5b8
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-03-07 14:03:20 -0700

    Update main.tf

--- iam-role-eks-v2/terraform/role/main.tf
+++ iam-role-eks-v2/terraform/role/main.tf
@@ -61,7 +61,7 @@ output "role_id" {
 variable "region" {}
 variable "access_key" {}
 variable "secret_key" {}
-variable "terraform_assume_role_arn" {}
+variable "assume_role_arn" {}
 
 terraform {
   required_providers {
@@ -76,7 +76,7 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   assume_role {
-    role_arn = var.terraform_assume_role_arn
+    role_arn = var.assume_role_arn
   }
 }
 
